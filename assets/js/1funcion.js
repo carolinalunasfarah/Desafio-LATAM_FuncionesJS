ea98f8c32b8c4d5bda263ca259e5c53596b8402d
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-06-28 10:15:54 -0400

    adjustments

--- assets/js/1funcion.js
+++ assets/js/1funcion.js
@@ -2,6 +2,7 @@ function example(a, b, c) {
     return a + b + c;
 }
 
+// Expression function
 example = function (a,b,c) {
     return a + b + c
 }
--- assets/js/2arrow.js
+++ assets/js/2arrow.js
@@ -2,4 +2,5 @@ suma = function (a, b) {
     return a + b;
 };
 
+// Arrow function
 suma = (a,b) => {return (a + b)}
