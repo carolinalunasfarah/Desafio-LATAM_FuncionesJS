f58575f8441a8a61d9607337127e9775251801e0
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-06-28 10:51:28 -0400

    3rd exercise ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 let ele = document.getElementById("ele1");
 
+function pintar (color = "green"){
+    elemento = document.getElementById("ele1");
+    ele.style.backgroundColor = color;
+}
+
+pintar ();
+
 ele.addEventListener("click", function pintar() {
     ele.style.backgroundColor = "yellow";
 });
