04c67df446cc5df4da941a5c442b2c066fc127c9
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-06-30 19:13:55 -0400

    keydown js

--- assets/js/4colores.js
+++ assets/js/4colores.js
@@ -1,11 +1,25 @@
-let dblue = document.getElementById ("1blue");
-let dred = document.getElementById ("2red");
-let dgreen = document.getElementById ("3green");
-let dyellow = document.getElementById ("4yellow");
+let dblue = document.getElementById("1blue");
+let dred = document.getElementById("2red");
+let dgreen = document.getElementById("3green");
+let dyellow = document.getElementById("4yellow");
 let dkey = document.getElementById("key");
 
 let blackbg = (changebg) => {
     changebg.target.style.backgroundColor = "black";
-}
+};
 
-dred.addEventListener("click", (blackbg))
+dblue.addEventListener("click", blackbg);
+dred.addEventListener("click", blackbg);
+dgreen.addEventListener("click", blackbg);
+dyellow.addEventListener("click", blackbg);
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "a") {
+        dkey.style.backgroundColor = "pink";
+    } else if (event.key === "s") {
+        dkey.style.backgroundColor = "orange";
+    }
+    else if (event.key === "d") {
+        dkey.style.backgroundColor = "cyan";
+    }
+});
